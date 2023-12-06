791ff0a331a6fc37ee358c6cad5968cea1ceb7b9
Chuxiness <146590179+Chuxiness@users.noreply.github.com>
2023-12-06 05:25:01 +0000

    commiting after most test cases passed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,10 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def director
+    #return "Hello!"
+    the_id = self.director_id
+    matching_director = Director.where({ :id => the_id }).at(0)
+    return matching_director
+  end
 end
